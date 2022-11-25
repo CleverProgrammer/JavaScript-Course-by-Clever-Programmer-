79d3bbab589f4c9d6095363453bba2fba7dba2d9
loic242 <bmavah@gmail.com>
2022-11-25 09:29:03 +0200

    For Loop

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,1 +1,6 @@
-console.log("Loic");
+const myArray = ["Loic","Mathis","Bricia","Eudes","Eleo","Aime","Chany","Christ","Ivan"];
+//Let's use a For Loop to get all array's value
+
+for(let i=0; i < myArray.length; i++){
+    console.log(myArray[i]);
+}
